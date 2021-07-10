744cd3588a3390146a1bc4bc2b5eb777afab4068
SamArsanios <samson.kibrom@hotmail.com>
2021-07-10 13:41:20 +0300

    Create store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  return (
+  // useEffect(() => {
+  //   const apiKey = 'e5567719e7a9a7711ba2f297c92dcdbc';
+  //   const hash = '2329c78c574a4a57c85382a88fe60f63';
 
-    <div />
+  //   const url = `https://gateway.marvel.com/v1/public/comics?ts=1&apikey=${apiKey}&hash=${hash}`;
+
+  //   const fetchComics = async (url) => {
+  //     const response = await fetch(url);
+  //     const comics = await response.json();
+
+  //     console.log(comics);
+  //   };
+  //   fetchComics(url.results);
+  // }, []);
+
+  return (
+    <div>
+      something
+    </div>
   );
 }
 
