e1ab805e9f591d6bd0672ca027c92470eb045f1d
SamArsanios <samson.kibrom@hotmail.com>
2021-07-11 18:53:35 +0300

    Add footer

---
+++ src/components/presentation/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="footer p-3 mt-4 text-center bg-dark text-light">
+            Developed
+            <span className="text-warning font-weight-normal">
+              Sam
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
