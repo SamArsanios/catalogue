90dbc745701728e4dd650a3002c44c9632eef5ad
SamArsanios <samson.kibrom@hotmail.com>
2021-07-12 19:51:15 +0300

    Add Film component

---
+++ src/components/presentation/Film.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class Film extends Component {
+  render() {
+    const { film } = this.props;
+    return (
+      <div className="col-md-3 my-4">
+        <div className="card card-body bg-dark text-center h-100">
+          <img className="w-100 mb-2" src={film.Poster} alt="Film Cover" />
+          <h5 className="text-light card-title">
+            {film.Title}
+              -
+              {film.Year}
+          </h5>
+          <Link className="btn btn-warning mt-auto" to={'/film/' + film.imdbID}>
+            Film Details
+              <i className="fas fa-chevron-right" />
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Film;
