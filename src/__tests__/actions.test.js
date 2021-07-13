23b8afd5d410c80d2c7735cf75198f85b88ed56f
SamArsanios <samson.kibrom@hotmail.com>
2021-07-13 19:11:39 +0300

    modify tests

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -1,8 +1,25 @@
 import {
-    searchFilm, fetchMangaFailure, fetchMangaSuccess, changeFilter,
+    searchFilm, fetchFilms
 } from '../actions/index';
 
-test('fetchMangaBegin returns BEGIN', () => {
+// import { FETCH_FILMS } from '../actions/constants';
+
+test('fetchFilma returns SEARCH_FILM', () => {
     const text = { title: 'blah', author: 'blah blah' };
     expect(searchFilm(text)).toEqual({ type: 'SEARCH_FILM', text });
+});
+
+const {
+    FETCH_FILMS,
+} = actions;
+
+describe('fetchFilms', () => {
+    it('sets the state propery film1s to a new array', () => {
+        const films = [{ film: 'film1' }, { film: 'film2' }];
+        const expectation = {
+            type: FETCH_FILMS,
+            films,
+        };
+        expect(fetchFilms(films)).toStrictEqual(expectation);
+    });
 });
