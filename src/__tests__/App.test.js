9c6132a61cd500451993aba3ce78ef95abcf517a
SamArsanios <samson.kibrom@hotmail.com>
2021-07-14 12:55:35 +0300

    Write tests for App component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import create from 'react-test-renderer';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import PropTypes from 'prop-types';
 import App from '../App';
+import { rootReducer } from '../reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-test('renders correctly', () => {
-  const tree = create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
+const state = {};
+const middleware = [thunk];
+const store = createStore(rootReducer, state, composeWithDevTools(applyMiddleware(...middleware)),);
+const Wrapper = ({ children }) => (
+  <Provider store={store}>{children}</Provider>
+);
+
+Wrapper.propTypes = {
+  children: PropTypes.shape({}).isRequired,
+};
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const tree = render(<App />, { wrapper: Wrapper });
+    expect(tree).toMatchSnapshot();
+  });
 });
