e8e676ad71605c26a8cd647819dc4102f4fa3758
SamArsanios <samson.kibrom@hotmail.com>
2021-07-14 12:57:37 +0300

    Add SearchForm test

---
+++ src/__tests__/SearchForm.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import PropTypes from 'prop-types';
+import SearchForm from '../components/containers/SearchForm';
+import { rootReducer } from '../reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const state = {};
+const middleware = [thunk];
+const store = createStore(rootReducer, state, composeWithDevTools(applyMiddleware(...middleware)),);
+const Wrapper = ({ children }) => (
+  <Provider store={store}>{children}</Provider>
+);
+
+Wrapper.propTypes = {
+  children: PropTypes.shape({}).isRequired,
+};
+
+describe('Film', () => {
+  it('renders correctly', () => {
+    const tree = render(<SearchForm />, { wrapper: Wrapper });
+    expect(tree).toMatchSnapshot();
+  });
+});
