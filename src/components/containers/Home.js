5060fc750c8a3b8976251ef795afa6ff91e97d64
SamArsanios <samson.kibrom@hotmail.com>
2021-07-17 00:39:18 +0300

    Render paginations component

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import SearchForm from './SearchForm';
 import FilmContainer from './FilmContainer';
 import Spinner from '../presentation/Spinner';
 import Pagination from '../presentation/Pagination';
+import { fetchFilms } from '../../actions/index';
 
 const Home = (props) => {
+  const [page, setPage] = useState(1);
   const { loading } = props;
+
+  useEffect(() => {
+    props.fetchFilms(props.text, page);
+  }, [page]);
+
+  const handleNextClick = () => {
+    setPage((page) => page + 1);
+  };
+
+  const handlePreviousClick = () => {
+    setPage((page) => (page === 1 ? page : page - 1));
+  };
 
   return (
     <div className="container">
       <SearchForm />
       {loading ? <Spinner /> : <FilmContainer />}
-      <Pagination />
+      <Pagination handleNextClick={handleNextClick} handlePreviousClick={handlePreviousClick} />
     </div>
   );
 };
@@ -24,10 +38,13 @@ const mapStateToProps = (state) => ({
 
 Home.propTypes = {
   loading: PropTypes.bool,
+  text: PropTypes.string,
+  fetchFilms: PropTypes.func.isRequired,
 };
 
 Home.defaultProps = {
   loading: false,
+  text: false,
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, { fetchFilms })(Home);
